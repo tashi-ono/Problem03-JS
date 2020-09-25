0771e6cb86dfcbe5cd46df38a8ae6acbacb48b80
Kaleb Burd <kalebmburd@gmail.com>
2020-09-24 22:10:39 -0400

    Update ar.test.js

--- ar.test.js
+++ ar.test.js
@@ -10,8 +10,9 @@ test("Test uno", () =>{
 });
 
 test("Test dos", () =>{
+    let rotator = new ArrayRotator();
     let rotateAmount = 10;
     let actual = rotator.rotateLeft([41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51], rotateAmount);
-    let expected = [77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 7];
+    let expected = [77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77];
     expect(expected).toEqual(actual);
 });
